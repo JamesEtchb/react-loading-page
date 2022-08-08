3d289e85f0fbf6fda600fbe3bb643bc9bc833e5b
james etchberger <etchbergerjames@gmail.com>
2022-08-08 11:49:45 -0400

    conditional rendering made a loading page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import Loading from './components/Loading.js'
+import Loaded from './components/Loaded.js'
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [objectExample, setObjectExample] = useState({ name:'name of object' })
+  const [userName, setUserName] = useState('')
+
+  const ternary = isLoaded ? 'YEP!' : 'NO 😢'
+  const numbers = [1,2,3,4,5,6,7,8,9,0]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="App">
+        <h1>Username is: {userName !== '' ? userName : 'Guest'}</h1>
+        <h1>{'Username is: ' + userName || 'Guest'}</h1> 
+        <h1>{
+          isLoaded && 
+          <>
+          {numbers.map((num) => {
+            return num + ' & '
+          }).reverse()}
+          <Loaded />
+          <h2> I have {numbers.length} numbers</h2>
+          </>
+        }</h1>
+        {/* <input onClick={setUserName()}></input> */}
+
+        <button onClick={() => {
+          console.log('button clicked')
+          setIsLoaded(!isLoaded)
+        }}>Change Loaded</button>
+        <h1>{objectExample.name}</h1>
+        <h1>{ternary}</h1>
+
+    {
+      isLoaded ?
+      <Loaded />
+      :
+      <Loading />
+    }   
+    <h1>isLoaded = {String(isLoaded)}</h1>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+import logo from '../logo.svg'
+
+function Loading() {
+  return (
+    <>
+      <img src='http://cdn.onlinewebfonts.com/svg/img_462420.png' className="App-logo" />
+      <h1>Loading ...</h1>
+      <img style={{width:250}} src='https://miro.medium.com/max/825/1*kNm9rtz-Ud5jmP9qGynmog.png' />
+    </>
+  )
+}
+
+export default Loading
